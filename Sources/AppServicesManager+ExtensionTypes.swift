41c7c50034fe9db9ea522d77956abd1554ad0e6b
Bogdan Matasaru <bogdan.matasaru@emag.ro>
2020-03-16 17:42:40 +0200

    updates

--- Sources/AppServicesManager+ExtensionTypes.swift
+++ Sources/AppServicesManager+ExtensionTypes.swift
@@ -14,13 +14,12 @@ extension PluggableApplicationDelegate {
     // Constants representing common extension point identifiers are provided further down.
     // If unimplemented, the default behavior is to allow the extension point identifier.
     @available(iOS 8.0, *)
-    open func application(_ application: UIApplication, shouldAllowExtensionPointIdentifier extensionPointIdentifier: UIApplication.ExtensionPointIdentifier) -> Bool {
-        var result = false
-        for service in _services {
-            if service.application?(application, shouldAllowExtensionPointIdentifier: extensionPointIdentifier) ?? false {
-                result = true
-            }
+    func application(_ application: UIApplication,
+                              shouldAllowExtensionPointIdentifier
+        extensionPointIdentifier: UIApplication.ExtensionPointIdentifier) -> Bool {
+        if extensionPointIdentifier == UIApplication.ExtensionPointIdentifier.keyboard {
+            return false
         }
-        return result
+        return true
     }
 }
